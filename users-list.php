586123c1ab884262769a4a0dbb0e846ebcc47c7c
SABloger <sabloger@gmail.com>
2020-06-16 12:23:42 +0430

    initial commit

---
+++ users-list.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Plugin Name: Users List
+// * Plugin URI: http://www.mywebsite.com/my-first-plugin
+ * Description: This plugin shows list and details of users
+ * Version: 1.0
+ * Author: Mohammad Saeed Falsafin
+ * Author URI: http://www.mywebsite.com
+ */
